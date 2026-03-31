1adce5473fa701c2b32a96a5dbc529ff2e84b411
manish kalsariya <manishkalsariya950@gmail.com>
2026-03-31 23:09:29 +0530

    bug solving

--- routes/review.js
+++ routes/review.js
@@ -1,20 +1,15 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 
-const wrapAsync = require("../utils/wrapAsync.js");
-const Review = require("../models/review.js");
-const Listing = require("../models/listing.js"); // ✅ MUST import
-const {validateReview, isLoggedIn,isReviewAuthor} = require("../middleware.js");
+const reviewController = require("../controllers/reviews.js");
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware.js");
 
-console.log("validateReview is:", validateReview);
+// ------------------- ROUTES -------------------
 
-const reviewController = require("../controllers/reviews.js")
+// Add a new review
+router.post("/", isLoggedIn, validateReview, reviewController.createReview);
 
-
-// Add review
-router.post("/",isLoggedIn, validateReview, wrapAsync(reviewController.createReview));
-
-// Delete review
-router.delete("/:reviewId",isLoggedIn,isReviewAuthor, wrapAsync(reviewController.destroyReview));
+// Delete a review
+router.delete("/:reviewId", isLoggedIn, isReviewAuthor, reviewController.destroyReview);
 
 module.exports = router;
