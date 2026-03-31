dabf27f27ea51f8279cfa882e9d051bda7a44f9b
manish kalsariya <manishkalsariya950@gmail.com>
2026-03-31 21:04:14 +0530

    n

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,23 +1,43 @@
-const Listing = require("../models/listing")
-const Review = require("../models/review")
+const Listing = require("../models/listing");
+const Review = require("../models/review");
+const ExpressError = require("../utils/ExpressError");
 
-module.exports.createReview = async (req, res) => {
+// Helper to wrap async routes and forward errors
+const wrapAsync = (fn) => {
+  return function(req, res, next) {
+    fn(req, res, next).catch(next);
+  };
+};
+
+module.exports.createReview = wrapAsync(async (req, res) => {
   const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    req.flash("error", "Listing not found!");
+    return res.redirect("/listings");
+  }
+
   const newReview = new Review(req.body.review);
   newReview.author = req.user._id;
   listing.reviews.push(newReview);
 
   await newReview.save();
   await listing.save();
+
   req.flash("success", "New Review added, thanks!");
   console.log("New review saved!");
-  res.redirect(`/listings/${req.params.id}`);
-}
+  return res.redirect(`/listings/${req.params.id}`);
+});
 
-module.exports.destroyReview = async (req, res) => {
+module.exports.destroyReview = wrapAsync(async (req, res) => {
   const { id, reviewId } = req.params;
-  await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+
+  const listing = await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+  if (!listing) {
+    req.flash("error", "Listing not found!");
+    return res.redirect("/listings");
+  }
+
   await Review.findByIdAndDelete(reviewId);
   req.flash("success", "Review deleted!");
-  res.redirect(`/listings/${id}`);
-}
+  return res.redirect(`/listings/${id}`);
+});
