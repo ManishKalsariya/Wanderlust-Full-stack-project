4111e1730d7f6002600c3b5f5c7c1c7c36eac779
manish kalsariya <manishkalsariya950@gmail.com>
2026-03-31 11:49:31 +0530

    fixing some bugs

--- controllers/users.js
+++ controllers/users.js
@@ -8,30 +8,39 @@ module.exports.renderLogInForm = (req,res)=>{
     res.render("users/login.ejs")
 }
 
-module.exports.signUp = async (req,res)=>{
-    try{
+module.exports.signUp = async (req, res, next) => {
+    try {
+        const { username, email, password } = req.body;
 
-        let {username, email, password} = req.body;
-        const newUser = new user({email,username});
+        if (!username || !email || !password) {
+            req.flash("error", "All fields are required!");
+            return res.redirect("/signup");
+        }
+
+        const newUser = new user({ username, email });
         const registeredUser = await user.register(newUser, password);
-        req.login(registeredUser, (err)=>{
-            if(err){
-                return next(err);
-            }
 
-            req.flash("success", "welcome to Wanderlust !");
-            res.redirect("/listings");
-            
-        })
-    }catch(e){
-        req.flash("error",e.message);
-        res.redirect("/signup");    }
-}
+        req.login(registeredUser, (err) => {
+            if (err) return next(err);
 
-module.exports.logIn = async (req,res)=>{
-    req.flash("success","welcome back to wanderLust !");
-    res.redirect(res.locals.redirectUrl || "/listings");
-}
+            req.flash("success", "Welcome to Wanderlust!");
+            req.session.save(() => {
+                res.redirect("/listings");
+            });
+        });
+
+    } catch (e) {
+        req.flash("error", e.message);
+        res.redirect("/signup");
+    }
+};
+
+module.exports.logIn = (req, res) => {
+    req.flash("success", "Welcome back to Wanderlust!");
+    req.session.save(() => {
+        res.redirect("/listings");
+    });
+};
 
 
 
